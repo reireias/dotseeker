2dd3874b51aa38424caebd4dc4b0024cb3c9abfb
reireias <reireias@gmail.com>
2019-08-21 16:18:28 +0900

    refactor

--- GitHub.js
+++ GitHub.js
@@ -1,11 +1,13 @@
 const axios = require('axios')
 
-const API_BASE = 'https://api.github.com'
+const API_BASE = 'https://api2.github.com'
 const SEARCH_URL = `${API_BASE}/search/repositories`
+const LIMIT_URL = `${API_BASE}/rate_limit`
 
 module.exports = class GitHub {
   constructor(token) {
     this.token = token
+    this.headers = { Authorization: `token ${this.token}` }
   }
 
   async search(params = {}) {
@@ -17,6 +19,25 @@ module.exports = class GitHub {
       headers: headers,
       params: params
     })
+    return res.data
+  }
+
+  async tree(treesUrl, branch, params = {}) {
+    const url = treesUrl.replace('{/sha}', `/${branch}`)
+    const res = await axios.get(url, {
+      headers: this.headers,
+      params: params
+    })
+    return res.data
+  }
+
+  async blob(url) {
+    const res = await axios.get(url, { headers: this.headers })
+    return res.data
+  }
+
+  async rateLimit() {
+    const res = await axios.get(LIMIT_URL, { headers: this.headers })
     return res.data
   }
 }
--- Seeker.js
+++ Seeker.js
@@ -1,30 +1,6 @@
-const axios = require('axios')
 const path = require('path')
 const fs = require('fs')
 const GitHub = require('./GitHub.js')
-
-const API_BASE = 'https://api.github.com'
-const LIMIT_URL = `${API_BASE}/rate_limit`
-
-const addTokenHeader = options => {
-  if (!options) {
-    options = {}
-  }
-
-  if (process.env.GITHUB_API_TOKEN) {
-    if (!options.headers) {
-      options.headers = {}
-    }
-    options.headers.Authorization = `token ${process.env.GITHUB_API_TOKEN}`
-  }
-  return options
-}
-
-const get = async (url, options) => {
-  options = addTokenHeader(options)
-  const res = await axios.get(url, options)
-  return res
-}
 
 const exist = path => {
   try {
@@ -47,50 +23,18 @@ const writeFile = (path, buffer) => {
   }
 }
 
-const searchFiles = async (treesUrl, fullName) => {
-  const params = {
-    recursive: 1,
-    type: 'blob',
-    page: 1,
-    per_page: 100
-  }
-  const res = await get(treesUrl, { params: params })
-  const targetFiles = res.data.tree.filter(
-    item =>
-      item.type === 'blob' &&
-      /^.*\.?(bashrc|bash_profile|zshrc|zsh_profile)/.test(item.path)
-  )
-  for (let file of targetFiles) {
-    try {
-      const saveFilePath = `files/${fullName}/${file.path}`
-      const dir = path.dirname(saveFilePath)
-      createDirectory(dir)
-      const blobRes = await get(file.url)
-      writeFile(saveFilePath, Buffer.from(blobRes.data.content, 'base64'))
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  return targetFiles.length
-}
-
-const rateLimit = async () => {
-  const res = await get(LIMIT_URL, {})
-  return res.data
-}
-
 module.exports = class Seeker {
   constructor(options = {}) {
     this.q = options.q === undefined ? 'topic:dotfiles' : options.q
     this.sort = options.sort === undefined ? 'stars' : options.sort
     this.page = options.page === undefined ? 1 : options.page
     this.perPage = options.perPage === undefined ? 10 : options.perPage
+    this.gh = new GitHub(process.env.GITHUB_API_TOKEN)
   }
 
   async seek() {
     console.info('search dotfiles topic repositories...')
-    const gh = new GitHub(process.env.GITHUB_API_TOKEN)
-    const data = await gh.search({
+    const data = await this.gh.search({
       q: this.q,
       sort: this.sort,
       page: this.page,
@@ -103,15 +47,40 @@ module.exports = class Seeker {
         this.page}`
     )
     let count = 0
+    const params = {
+      recursive: 1,
+      type: 'blob',
+      page: 1,
+      per_page: 100
+    }
     for (let item of data.items) {
-      const treesUrl = item.trees_url.replace(
-        '{/sha}',
-        `/${item.default_branch}`
+      const data = await this.gh.tree(
+        item.trees_url,
+        item.default_branch,
+        params
       )
-      count += await searchFiles(treesUrl, item.full_name)
+      count += await this._searchFiles(data.tree, item.full_name)
     }
     console.info(`saved files: ${count}`)
     console.info('limit rate info:')
-    console.info(await rateLimit())
+    console.info(await this.gh.rateLimit())
+  }
+
+  async _searchFiles(tree, fullName) {
+    const targetFiles = tree.filter(item =>
+      /^.*\.?(bashrc|bash_profile|zshrc|zsh_profile)/.test(item.path)
+    )
+    for (let file of targetFiles) {
+      try {
+        const saveFilePath = `files/${fullName}/${file.path}`
+        const dir = path.dirname(saveFilePath)
+        createDirectory(dir)
+        const blob = await this.gh.blob(file.url)
+        writeFile(saveFilePath, Buffer.from(blob.content, 'base64'))
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    return targetFiles.length
   }
 }
