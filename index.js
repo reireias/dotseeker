10aa0472916511521b383ed48919198602531b21
reireias <reireias@gmail.com>
2020-06-04 16:21:29 +0900

    fix lint error

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ dotenv.config()
 if (!process.env.GITHUB_API_TOKEN) {
   console.error('Set GITHUB_API_TOKEN in .env file or environment.')
 } else {
-  new Seeker().seek().catch(e => console.error(e))
+  new Seeker().seek().catch((e) => console.error(e))
 }
